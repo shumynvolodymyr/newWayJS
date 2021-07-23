c62fb741f7ec8aabdcb7411f042cb3798eed8c87
shumynvolodymyr <shumivova@gmail.com>
2021-07-23 03:58:28 +0300

    additional 5

---
+++ additional/lesson5.js
@@ -0,0 +1,212 @@
+//
+// - Створити форму з інпутом для введення ім'я, та action="index2.html".
+// При відправці данних з форми, зберігати ім'я в localstorage, та переходити на index2.html.
+// На index2.html виводити записане в localstorage ім'я в div
+
+// const formForName = document.forms.formForName;
+// const name = formForName.name;
+// const submit = formForName.submit;
+//
+// submit.onclick = function (ev) {
+//     localStorage.setItem('name', name.value);
+// }
+
+////---------------------------------------------------------------------------------------
+
+// - Імітуємо наповнення інтернет магазину товарами :
+//     Створити форму з наступними полями :
+//     - назва товару
+// - кількість товару
+// - ціна товару
+// - картинка товару (посилання з інтернету)
+// Зберігати товари в масив в локалсорадж. При збережені товару з форми, action не повинно відбуватись.
+//     створити елемент <a href='list.html'> На сторінку товарів</a>, та list.html, при переході на який відобразити на сторінці всі товари.
+// На сторінці  list.html побудувати кнопку яка видаляє всі товари з корзини та локалстораджа.
+//     До кожного товару додати кнопку, при кліку на яку з лс видаляється конкретний обраний  товар
+
+const formForProduct = document.forms.formForProduct;
+const name = formForProduct.nameProduct;
+const quantity = formForProduct.quantityProduct;
+const price = formForProduct.priceProduct;
+const image = formForProduct.imageProduct;
+const submit = formForProduct.submit;
+
+let productList = [];
+
+formForProduct.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    let product = {
+        name: name.value,
+        quantity: quantity.value,
+        price: price.value,
+        image: image.value
+    };
+    productList.push(product);
+    localStorage.setItem('products', JSON.stringify(productList));
+});
+
+
+// 1- З масиву users за допомогою циклу витягнути адреси користувачів і записати (скопіювати) їх в інший порожній масив.
+// 2- За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement. Всі данні в одному блоці.
+// 3- За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам (div>div*4)
+// 4- За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам , блок з адресою зробити окремим блоком, з блоками для кожної властивості
+
+let users = [{
+    name: 'vasya',
+    age: 31,
+    status: false,
+    address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+}, {
+    name: 'petya',
+    age: 30,
+    status: true,
+    address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+}, {
+    name: 'kolya',
+    age: 29,
+    status: true,
+    address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+}, {
+    name: 'olya',
+    age: 28,
+    status: false,
+    address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+}, {
+    name: 'max',
+    age: 30,
+    status: true,
+    address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+}, {
+    name: 'anya',
+    age: 31,
+    status: false,
+    address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+}, {
+    name: 'oleg',
+    age: 28,
+    status: false,
+    address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+}, {
+    name: 'andrey',
+    age: 29,
+    status: true,
+    address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+}, {
+    name: 'masha',
+    age: 30,
+    status: true,
+    address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+}, {
+    name: 'olya',
+    age: 31,
+    status: false,
+    address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+}, {
+    name: 'max',
+    age: 31,
+    status: true,
+    address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+}];
+
+////------1-----------
+
+// let usersAdress = [];
+// for (const user of users) {
+//     usersAdress.push(user.address);
+// }
+// console.log(usersAdress);
+
+////------2-----------
+
+// for (const user of users) {
+// let div = document.createElement('div');
+//     for (const item in user) {
+//         document.body.append(div);
+//         div.append(`${item}: ${user[item]}`);
+//
+//         //// div.innerText = `${user.name} ${user.age} ${user.status} ${JSON.stringify(user.address)}`
+//     }
+// }
+
+////------3-----------
+
+// for (const user of users) {
+//     const div = document.createElement('div');
+//     document.body.append(div);
+//     for (const item in user) {
+//         const divka = document.createElement('div');
+//         div.append(divka);
+//         divka.innerHTML = `${item}: ${JSON.stringify(user[item])}`
+//     }
+// }
+
+////------4-----------
+
+// for (const user of users) {
+//     const div = document.createElement("div");
+//     const divName = document.createElement('div');
+//     const divAge = document.createElement('div');
+//     const divStatus = document.createElement('div');
+//     const divAddress = document.createElement('div');
+//
+//     for (const element in user.address) {
+//         const divka = document.createElement('div');
+//         divka.append(user.address[element]);
+//         divAddress.append(divka)
+//     }
+//
+//     document.body.appendChild(div);
+//     divName.append(user.name);
+//     divAge.append(user.age);
+//     divStatus.append(user.status);
+//
+//     div.append(divName);
+//     div.append(divAge);
+//     div.append(divStatus);
+//     div.append(divAddress);
+// }
+
+
+// - є сторінка rules.html. Контентом сторінки є заголовки та параграфи. Заголовки (h2) характеризують тему контенту яка вказана в параграфі.
+//     створити скріпт, котрий зчитує всі заголовки, та робить в блоці з id=content з них список(ul>li), який буде змістом того, що знаходиться на сторінці.
+//     Скріпт повинен працювати навіть якщо кількість блоків з заголовком та параграфом зміниться.
+//
+// -Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило в окремому блоці.
+//     При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
+//     Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
+//
+// let rules = [
+//     {
+//         title: 'Первое правило Бойцовского клуба.',
+//         body: 'Никому не рассказывать о Бойцовском клубе.'
+//     },
+//     {
+//         title: 'Второе правило Бойцовского клуба.',
+//         body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+//     },
+//     {
+//         title: 'Третье правило Бойцовского клуба.',
+//         body: 'В схватке участвуют только двое.'
+//     },
+//     {
+//         title: 'Четвертое правило Бойцовского клуба.',
+//         body: 'Не более одного поединка за один раз.'
+//     },
+//     {
+//         title: 'Пятое правило Бойцовского клуба.',
+//         body: 'Бойцы сражаются без обуви и голые по пояс.'
+//     },
+//     {
+//         title: 'Шестое правило Бойцовского клуба.',
+//         body: 'Поединок продолжается столько, сколько потребуется.'
+//     },
+//     {
+//         title: 'Седьмое правило Бойцовского клуба.',
+//         body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+//     },
+//     {
+//         title: 'Восьмое и последнее правило Бойцовского клуба.',
+//         body: 'Новичок обязан принять бой.'
+//     },
+//
+// ];
